ce601d82ae2d6daba66b556ab72fcf7a783fd8d7
richardse08 <eric.richards.finance@gmail.com>
2017-07-01 18:16:40 -0400

    Renamed some variables and cleared up some spacing to make code more readable

--- script.js
+++ script.js
@@ -1,123 +1,71 @@
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
-    
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-    var userLat = (position.coords.latitude);
-    var userLon = (position.coords.longitude);
-//end of geolocation code
 
+    
+    
+    // Get geolocation
+    if (navigator.geolocation) {
+        
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var localLat = (position.coords.latitude);
+            var localLon = (position.coords.longitude);
+
+            var localLatLon = "http://api.openweathermap.org/data/2.5/weather?lat=" + localLat + "&lon=" + localLon + "&APPID=70fa491fc35f6904d716e3250636eac4";    
+            var weatherRequest = new XMLHttpRequest();
+            weatherRequest.open('GET', localLatLon);
+            weatherRequest.onload = function() {
+
+                var localData = JSON.parse(weatherRequest.responseText);
+                var localTempLongF = localData.main.temp*(9/5)-459.67 + " is Your Local Temperature in ";
+                var localTempLongC = localData.main.temp-273 + " is Your Local Temperature in ";
+                var localTempF = localTempLongF.substr(0,4);
+                var localTempC = localTempLongC.substr(0,4);
+                var localSpeed = localData.wind.speed + " is Your Local Wind Speed in Knots";
+                var localHumidity = localData.main.humidity + " is Your Local Humidity in %" 
+                var localIcon = localData.weather[0].icon;  
+                var localIconImg = "http://openweathermap.org/img/w/" + localIcon + ".png"
+                var link = "<img src='" + localIconImg + "'>";
+                var localF = localTempF + "F Degrees";
+                var localC = localTempC + "C Degrees";
+                var localArr = [localF,localC];
+                var local = [];
+                $("#tempTarget").html(localTempF);
+                $("#speedTarget").html(localSpeed);
+                $("#humidityTarget").html(localHumidity);
+                $("#iconTarget").html(localIconImg);    
+                $("#icon").html(link);
+                $("#loadingTarget").html("Uplink Successful! Click Below to Toggle Between Fahrenheit and Celsius!");
+
+                // If button is clicked, reveal temp in F
+                $('#myButton').click(function(){
+
+                    $('.showF').html(localArr[0]);
+
+                });
+                
+                // If button click count is odd, show F; if click count is even, show C
+                $('#myButton').click(function() {
+
+                    local++;
+                    if (local % 2 === 1) {
+                        $('.showF').html(localArr[0]);
+                    } else {$('.showF').html(localArr[1])};
+
+                });
+
+            }; // End weatherRequest
+            
+            
+            
+            // Fire AJAX
+            weatherRequest.send(); 
+            
+            
+        }); // End getCurrentPosition function
         
         
-var userLatLon = 
-    "http://api.openweathermap.org/data/2.5/weather?lat=" + userLat + "&lon=" + userLon + "&APPID=70fa491fc35f6904d716e3250636eac4";    
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', userLatLon);
-ourRequest.onload = function() {
-    var rickData = JSON.parse(ourRequest.responseText);
-   
-    var rickTempLongF = 
-        rickData.main.temp*(9/5)-459.67 + " is Your Local Temperature in ";
-    var rickTempLongC = 
-        rickData.main.temp-273 + " is Your Local Temperature in ";
-    var rickTempF = rickTempLongF.substr(0,4);
-    var rickTempC = rickTempLongC.substr(0,4);
-    var rickSpeed = 
-        rickData.wind.speed + " is Your Local Wind Speed in Knots";
-    var rickHumidity = 
-        rickData.main.humidity + " is Your Local Humidity in %" 
-    var rickIcon = rickData.weather[0].icon;  
-    var rickIconImg = 
-        "http://openweathermap.org/img/w/" + rickIcon + ".png"
-    //end of extrapolation
-
-
-
-    $("#tempTarget").html(rickTempF);
-    $("#speedTarget").html(rickSpeed);
-    $("#humidityTarget").html(rickHumidity);
-    $("#iconTarget").html(rickIconImg);    
-            var link = "<img src='" + rickIconImg + "'>";
-    $("#icon").html(link);
-    $("#loadingTarget").html("Uplink Successful! Click Below to Toggle Between Fahrenheit and Celsius!");
-
+    } // End if(navigator.geolocation)
 
     
     
-    
-    
-    
-    
-    var testF = rickTempF + "F Degrees";
-    var testC = rickTempC + "C Degrees";
-    var testArr = [testF,testC];
-
-
-    $('#myButton').click(function(){
-        $('.showF').html(testArr[0]);
-    });  
-    
-//    $('#myButton2').click(function(){
-//        $('.showC').html(testArr[1]);
-//    });   
-////////////////////////////////////////////////////
-var test = [];
-    
-$('#myButton').click(function() {
-    test++;
-    if (test % 2 === 1) {
-        $('.showF').html(testArr[0]);
-    } else {$('.showF').html(testArr[1])};
-
-
-});
-    
-
-    
-    
-    console.log(rickData);
-    
-    
-    
-    
-        
-        
-};
-ourRequest.send();        
-     });
-  }   
-    
- 
-
-    
-
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-    
-
        
 }); //close $(document).ready         
