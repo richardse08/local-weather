a1d265347cb61d28c259e89390450d88a4c487bd
richardse08 <eric.richards.finance@gmail.com>
2017-08-05 11:14:33 -0400

    Created images folder and added some general commenting

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # local-weather
 The WeatherApp utilizes geolocation and API calls to determine and display user's local weather conditions
+
+This project demonstrates using AJAX to request and retrieve a simple JSON object
+
+Please note that the local weather will not update if viewing through CodePen
--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 $(document).ready(function() {
 
-    
-    
-    // Get geolocation
+    // Get access to location of device
     if (navigator.geolocation) {
         
         navigator.geolocation.getCurrentPosition(function(position) {
+            
+            
+            
+            // Create variables to use users latitude and longitude
             var localLat = (position.coords.latitude);
             var localLon = (position.coords.longitude);
 
+            // Create string for AJAX request
             var localLatLon = "http://api.openweathermap.org/data/2.5/weather?lat=" + localLat + "&lon=" + localLon + "&APPID=70fa491fc35f6904d716e3250636eac4";    
+            
+            // Define our new request for the local weather
             var weatherRequest = new XMLHttpRequest();
+            
+            // Creating get request and function
             weatherRequest.open('GET', localLatLon);
+            
+            
             weatherRequest.onload = function() {
-
+                
+                // Could have made this a lot shorted by creating just a couple of variables
                 var localData = JSON.parse(weatherRequest.responseText);
                 var localTempLongF = localData.main.temp*(9/5)-459.67 + " is Your Local Temperature in ";
                 var localTempLongC = localData.main.temp-273 + " is Your Local Temperature in ";
@@ -28,6 +38,8 @@ $(document).ready(function() {
                 var localC = localTempC + "C Degrees";
                 var localArr = [localF,localC];
                 var local = [];
+                
+                // Select elements and update with data from server response
                 $("#tempTarget").html(localTempF);
                 $("#speedTarget").html(localSpeed);
                 $("#humidityTarget").html(localHumidity);
@@ -35,15 +47,15 @@ $(document).ready(function() {
                 $("#icon").html(link);
                 $("#loadingTarget").html("Uplink Successful! Click Below to Toggle Between Fahrenheit and Celsius!");
 
-                // If button is clicked, reveal temp in F
-                $('#myButton').click(function(){
+                // If button is clicked, initially reveal temp in F
+                $('#weather_btn').click(function(){
 
                     $('.showF').html(localArr[0]);
 
                 });
                 
                 // If button click count is odd, show F; if click count is even, show C
-                $('#myButton').click(function() {
+                $('#weather_btn').click(function() {
 
                     local++;
                     if (local % 2 === 1) {
@@ -52,12 +64,13 @@ $(document).ready(function() {
 
                 });
 
-            }; // End weatherRequest
+                
+            }; // End weatherRequest.onload
             
-            
-            
-            // Fire AJAX
+            // Send request to server
             weatherRequest.send(); 
+            
+            
             
             
         }); // End getCurrentPosition function
@@ -66,6 +79,4 @@ $(document).ready(function() {
     } // End if(navigator.geolocation)
 
     
-    
-       
 }); //close $(document).ready         
